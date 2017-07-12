484d448feff32bc28cd0b9ac9e5e703e2845d688
Mikhail Metrikin <metrikin@gmail.com>
2017-07-11 22:47:29 -0700

    Refreshing C++ fundamentals

---
+++ InputOutput.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    int x;
+
+    // Prompt for a number and then load into x
+    std::cout << "Enter a phone number: ";
+    std::cin >> x;
+    std::cout << "You entered " << x << std::endl;
+    return 0;
+}
